a7d90dfed41bb833b3e5317273a6b5254bb37f1d
MoisesDorado <dorado.gumoi24@triana.salesianos.edu>
2024-05-07 14:17:13 +0200

    asdasd

--- springtoolBBDD_workspace/ServicioBase/src/main/java/com/salesianostriana/dam/serviciobase/repositorio/AlumnoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.serviciobase.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.serviciobase.modelo.Alumno;
-
-public interface AlumnoRepositorio extends JpaRepository<Alumno, Long> {
-	
-}
